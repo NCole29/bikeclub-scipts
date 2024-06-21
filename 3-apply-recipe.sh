0703da94ab58279933520ec6e83217550bd12944
NCole29 <59418813+NCole29@users.noreply.github.com>
2024-06-21 17:48:01 -0400

    Create 3-apply-recipe.sh

---
+++ 3-apply-recipe.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# This script should be stored and run from the sites folder with the command:
+# ./3-apply-recipe.sh
+
+read -p "Enter project folder name: " folder
+read -p "Enter recipe name: bikeclub-" recipe
+
+cd $folder
+
+# Add bikeclub recipe repository to composer.json.
+echo '<----------- Add recipe repository to composer.json -------------->'
+ddev composer config repo.bikeclub-$recipe git "https://github.com/NCole29/bikeclub-$recipe"
+
+# Composer require (download) bikeclub recipe repository.
+echo '<----------- Download recipe repository to recipes/contrib -------------->'
+ddev composer require bikeclub/bikeclub-$recipe:"^1.0"
+ddev drush cr
+
+# Apply the bikeclub recipe to Drupal website.
+echo '<----------- Apply the bikeclub recipe -------------->'
+ddev exec --dir /var/www/html/web php core/scripts/drupal recipe recipes/contrib/bikeclub-$recipe 
+ddev drush cr
+
+echo '<----------- Job completed -------------->'
+echo ' Refresh your browser. If the site returns an error:'
+echo '  - Go to the project folder in your terminal window and clear the cache: ddev drush cr'
